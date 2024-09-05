613c613a5f1899643f0b418bf51dcdd30dfc0826
qwertyriks <romeokumatse@gmail.com>
2024-09-05 23:13:58 +0000

    In progress

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Module of Users views"""
+""" Module of Users views
+"""
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
@@ -7,8 +8,10 @@ from models.user import User
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
-    """GET /api/v1/users
-    Return:list of all User objects JSON represented"""
+    """ GET /api/v1/users
+    Return:
+      - list of all User objects JSON represented
+    """
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
 
@@ -16,9 +19,12 @@ def view_all_users() -> str:
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
     """ GET /api/v1/users/:id
-    Path parameter:User ID
-    Return:- User object JSON represented
-      - 404 if the User ID doesn't exist """
+    Path parameter:
+      - User ID
+    Return:
+      - User object JSON represented
+      - 404 if the User ID doesn't exist
+    """
     if user_id is None:
         abort(404)
     user = User.get(user_id)
@@ -32,8 +38,10 @@ def delete_user(user_id: str = None) -> str:
     """ DELETE /api/v1/users/:id
     Path parameter:
       - User ID
-    Return- empty JSON is the User has been correctly deleted
-      - 404 if the User ID doesn't exist"""
+    Return:
+      - empty JSON is the User has been correctly deleted
+      - 404 if the User ID doesn't exist
+    """
     if user_id is None:
         abort(404)
     user = User.get(user_id)
